b59e320e8421f9e48082dc5a6f5bbaedee7b9213
Aleksandra <aleksandra.paperz@email.com>
2026-02-16 00:59:04 +0100

    Invoice: implementacja addProduct() i subtotal dla pojedynczych produktów

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,15 +1,16 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Collection<Product> products;
+    private Collection<Product> products = new ArrayList<>();
 
     public void addProduct(Product product) {
-        // TODO: implement
+        products.add(product);
     }
 
     public void addProduct(Product product, Integer quantity) {
@@ -17,7 +18,13 @@ public class Invoice {
     }
 
     public BigDecimal getSubtotal() {
-        return BigDecimal.ZERO;
+
+        BigDecimal subtotal = BigDecimal.ZERO;
+
+        for (Product product : products) {
+            subtotal = subtotal.add(product.getPrice());
+        }
+        return subtotal;
     }
 
     public BigDecimal getTax() {
@@ -25,6 +32,6 @@ public class Invoice {
     }
 
     public BigDecimal getTotal() {
-        return null;
+        return BigDecimal.ZERO;
     }
 }
