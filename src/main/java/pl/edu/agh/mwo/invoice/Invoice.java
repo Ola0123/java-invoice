d7ee446fa3c93ffff7ad3312e48de103e0f79615
Aleksandra <aleksandra.paperz@email.com>
2026-02-16 18:32:37 +0100

    Invoice: obsługa quantity w subtotal

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -3,26 +3,32 @@ package pl.edu.agh.mwo.invoice;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Collection<Product> products = new ArrayList<>();
+    private Map<Product, Integer> products = new HashMap<>();
 
     public void addProduct(Product product) {
-        products.add(product);
+        products.put(product, 1);
     }
 
     public void addProduct(Product product, Integer quantity) {
-        // TODO: implement
+        products.put(product, quantity);
     }
 
     public BigDecimal getSubtotal() {
 
         BigDecimal subtotal = BigDecimal.ZERO;
 
-        for (Product product : products) {
-            subtotal = subtotal.add(product.getPrice());
+        for (Map.Entry<Product, Integer> entry : products.entrySet()) {
+            Product product = entry.getKey();
+            Integer quantity = entry.getValue();
+
+            subtotal = subtotal.add(
+                    product.getPrice().multiply(BigDecimal.valueOf(quantity)));
         }
         return subtotal;
     }
